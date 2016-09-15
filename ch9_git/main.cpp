2bfd0d39d253645bad8e81a37d6d8f8e93e960ad
Shannon Smith <shannones2013@gmail.com>
2016-09-14 21:17:48 -0700

    Initial Commit

---
+++ ch9_git/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ch9_git
+//
+//  Created by Shannon Smith on 9/14/16.
+//  Copyright © 2016 Shannon Smith. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
